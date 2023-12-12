24e48677a5b795ecf96fa78907683f5391edade4
Temptreess <140977102+Temptreess@users.noreply.github.com>
2023-12-12 23:01:58 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def generate_random_filename(length=20):
     return ''.join(random.choice(letters) for _ in range(length))
 
 def generate_random_text():
-    text_length = random.randint(10, 10000000)
+    text_length = random.randint(100000, 100000000)
     letters = string.ascii_letters + string.digits + string.punctuation + string.whitespace
     return ''.join(random.choices(letters, k=text_length))
 
@@ -19,6 +19,7 @@ def create_random_file(file_count):
         text = generate_random_text()
         with open(filename + ".txt", 'w') as file:
             file.write(text)
+        print("Created:" + filename + ".txt")
 
 
 current_directory = os.getcwd()
